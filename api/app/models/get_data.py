4a9d0ebf6981a46a2ccdae16c25c9ad56e7ebb08
C4ncino <193305@iberopuebla.mx>
2024-10-28 19:44:26 -0600

    fix: prepare data functions

--- api/app/models/get_data.py
+++ api/app/models/get_data.py
@@ -12,22 +12,24 @@ def generate_random(quantity=20) -> np.ndarray:
     samples = []
 
     for _ in range(quantity):
-        first_five = np.random.randint(0, 6, size=(60, 5))
+        random_size = np.random.randint(54, 65)
 
-        last_three = np.round(np.random.uniform(-9.99, 10.0, size=(60, 3)), 2)
+        first_five = np.random.randint(0, 6, size=(random_size, 5))
+
+        last_three = np.round(np.random.uniform(-9.99, 10.0, size=(random_size, 3)), 2)
 
         samples.append(np.concatenate((first_five, last_three), axis=1))
 
-    samples_array = np.array(samples)
+    samples_array = np.array(samples, dtype=object)git
 
-    return samples_array.tolist()
+    return samples_array
 
 
 def prepare_data(sensor_data: list[dict], user_id: int) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     n_samples = len(sensor_data)
 
-    current_data = [pd.DataFrame(i).values for i in sensor_data]
-    current_labels = [1]*n_samples
+    current_data = np.array([pd.DataFrame(i).values for i in sensor_data], dtype=object)
+    current_labels = np.array([1]*n_samples)
 
     user_words = database.read_by_field('words', 'user_id', user_id)
 
@@ -42,12 +44,14 @@ def prepare_data(sensor_data: list[dict], user_id: int) -> tuple[np.ndarray, np.
             other_words_data.append(pd.DataFrame(json[i]).values)
 
     if len(other_words_data) > 0:
-        data = np.concatenate(
-            (current_data, np.array(other_words_data), generate_random())
-        )
-
         labels = np.concatenate(
             (current_labels, np.zeros(len(other_words_data)), np.zeros(20))
+        )
+
+        other_words_data = np.array(other_words_data, dtype=object)
+
+        data = np.concatenate(
+            (current_data, other_words_data, generate_random())
         )
 
     else:
@@ -77,7 +81,9 @@ def prepare_data_for_lm(user_id: int) -> tuple[np.ndarray, np.ndarray, np.ndarra
             training_data_arr.append(pd.DataFrame(sample).values)
             labels_arr.append(word.class_key)
 
-    data = np.concatenate((np.array(training_data_arr), generate_random()))
+    training_data = np.array(training_data_arr, dtype=object)
+
+    data = np.concatenate((training_data, generate_random()))
     labels = np.concatenate((np.array(labels_arr), np.zeros(20)))
 
     data = pad_sequences(data, maxlen=MAX_LEN, padding='post', dtype='float32')
