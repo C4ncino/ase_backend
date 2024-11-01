e4ff79214d4fd04cb73fa8fc45184f80024448eb
Nabila0416 <nabilamac2393@gmail.com>
2024-10-31 23:58:59 -0600

    get data

--- api/app/models/get_data.py
+++ api/app/models/get_data.py
@@ -1,26 +1,22 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from app.database import database
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-from app.database import database
-
-MAX_LEN = 60
-
+max_len= 60
 
 def generate_random(quantity=20) -> np.ndarray:
     samples = []
 
     for _ in range(quantity):
-        random_size = np.random.randint(54, 65)
+        first_five = np.random.randint(0, 6, size=(60, 5))
 
-        first_five = np.random.randint(0, 6, size=(random_size, 5))
-
-        last_three = np.round(np.random.uniform(-9.99, 10.0, size=(random_size, 3)), 2)
+        last_three = np.random.randint(-999, 1000, size=(60, 3))
 
         samples.append(np.concatenate((first_five, last_three), axis=1))
 
-    samples_array = np.array(samples, dtype=object)git
+    samples_array = np.array(samples)
 
     return samples_array
 
@@ -28,7 +24,7 @@ def generate_random(quantity=20) -> np.ndarray:
 def prepare_data(sensor_data: list[dict], user_id: int) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     n_samples = len(sensor_data)
 
-    current_data = np.array([pd.DataFrame(i).values for i in sensor_data], dtype=object)
+    current_data = np.array([pd.DataFrame(i).values for i in sensor_data])
     current_labels = np.array([1]*n_samples)
 
     user_words = database.read_by_field('words', 'user_id', user_id)
@@ -44,21 +40,20 @@ def prepare_data(sensor_data: list[dict], user_id: int) -> tuple[np.ndarray, np.
             other_words_data.append(pd.DataFrame(json[i]).values)
 
     if len(other_words_data) > 0:
-        labels = np.concatenate(
-            (current_labels, np.zeros(len(other_words_data)), np.zeros(20))
+        data = np.concatenate(
+            (current_data, np.array(other_words_data), generate_random())
         )
 
-        other_words_data = np.array(other_words_data, dtype=object)
-
-        data = np.concatenate(
-            (current_data, other_words_data, generate_random())
+        labels = np.concatenate(
+            (current_labels, np.zeros(len(other_words_data)), np.zeros(20))
         )
 
     else:
         data = np.concatenate((current_data, generate_random()))
         labels = np.concatenate((current_labels, np.zeros(20)))
 
-    data = pad_sequences(data, maxlen=MAX_LEN, padding='post', dtype='float32')
+    data = pad_sequences(data, maxlen=max_len, padding='post', dtype='float32')
+
 
     x_train, x_val, y_train, y_val = train_test_split(data, labels, test_size=0.2, stratify=labels)
 
@@ -81,12 +76,10 @@ def prepare_data_for_lm(user_id: int) -> tuple[np.ndarray, np.ndarray, np.ndarra
             training_data_arr.append(pd.DataFrame(sample).values)
             labels_arr.append(word.class_key)
 
-    training_data = np.array(training_data_arr, dtype=object)
-
-    data = np.concatenate((training_data, generate_random()))
+    data = np.concatenate((np.array(training_data_arr), generate_random()))
     labels = np.concatenate((np.array(labels_arr), np.zeros(20)))
 
-    data = pad_sequences(data, maxlen=MAX_LEN, padding='post', dtype='float32')
+    data = pad_sequences(data, maxlen=max_len, padding='post', dtype='float32')
 
     x_train, x_val, y_train, y_val = train_test_split(data, labels, test_size=0.2, stratify=labels)
 
